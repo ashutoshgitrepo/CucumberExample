28c91e32305f24c73960b0a57170b3a7c1eaa882
ashutoshgitrepo <47950195+ashutoshgitrepo@users.noreply.github.com>
2020-10-16 16:45:40 +0100

    Add files via upload

--- src/test/java/com/ashutosh/test/StepDefinition.java
+++ src/test/java/com/ashutosh/test/StepDefinition.java
@@ -1,5 +1,7 @@
 package com.ashutosh.test;
 
+import org.junit.Assert;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -18,6 +20,7 @@ public class StepDefinition {
 	public void user_enter_username_as_and_password_as(String arg1, String arg2) throws Throwable {// Write code here that turns the phrase above into concrete actions
 		System.out.println("Username is on login page"+arg1);
 		System.out.println("User passoed is on login page"+arg2);
+		
 	    //throw new PendingException();
 	}
 
@@ -38,4 +41,21 @@ public class StepDefinition {
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("User should be in login page");
 	}
+	
+	
+	@When("^when background step is executed$")
+	public void when_background_step_is_executed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("when background step is executed...");
+	}
+	@Given("^I want to verify that (\\d+) plus (\\d+) equals (\\d+)$")
+	public void i_want_to_verify_that_plus_equals(int arg1, int arg2, int arg3) {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("arg1="+ arg1);
+		System.out.println("arg2="+ arg2);
+		System.out.println("arg3="+ arg3);
+		//Assert.assertEquals("", arg1, 1, 0);
+	}
+	
+	
 }
